b506d3478a9a335190615629aa843cf2a285e0ed
Alexandr <sashka.ryzhik.19.08@gmail.com>
2026-08-06 01:24:51 +0500

    текст комментария

--- fit_life.py
+++ fit_life.py
@@ -1,4 +1,8 @@
-  #  Позволил себе немного вольности
+import sys
+
+if hasattr(sys.stdout, "reconfigure"):
+    sys.stdout.reconfigure(encoding="utf-8")
+
 print("Приветствую, я Ваш персональный фитнес-бот FitLife!")
 print("=" * 62)
 user_name = input("Пожалуйста, введите Ваше имя: ")
@@ -20,58 +24,25 @@ while True:  #  зациклил, по тому же принипу, что и �
 
 while True:  
     try:
-        user_height_sm = float(input("Введите Ваш рост в сантиметрах: "))
-        user_height = user_height_sm / 100
+        user_height = float(input("Введите Ваш рост в метрах: "))
         break
     except ValueError:
         print("Пожалуйста, введите рост, используя только цифры.")  #  я бы хотел добавитть возможность заменять запятую на точку, но пока не знаю как. 
-while True:  # зациклил, чтобы пользователь вводил нужный текст
-    user_sex = input("Eкажите Ваш пол (мужской/женский): ").lower() #  это пригодится далее
-    if user_sex == "мужской": 
-        user_sex = "male"
-        break
-    elif user_sex == "женский":
-        user_sex = "female"
-        break
-    else:
-        print("Пожалуйста, введите 'мужской' или 'женский'.")
 
 bmi = user_weight / (user_height ** 2) 
+
 user_bmi = round(bmi, 1) 
-if user_bmi < 18.5:
-    user_bmi_category = "недостаточный вес, вам нужно питаться более калорийно."
-elif 18.5 <= user_bmi < 24.9:
-    user_bmi_category = "нормальный вес, продолжайте поддерживать здоровый образ жизни." 
-elif 25 <= user_bmi < 29.9:
-    user_bmi_category = "избыточный вес, рекомендуется обратиться к диетологу."
-elif 30 <= user_bmi < 34.9:
-    user_bmi_category = "ожирение I степени, Вам необходимо контролировать питание и физическую активность."
-elif 35 <= user_bmi < 39.9:
-    user_bmi_category = "ожирение II степени, Вам необходимо контролировать питание и физическую активность."
-elif user_bmi >= 40:
-    user_bmi_category = """ожирение III степени, 
-Вам необходимо контролировать питание и физическую активность, если для Вас это проблема, 
-то рекомендуется обратиться к врачу."""
-def calc_ideal_weight(height, sex):  #  Формула Лоренца, используется для расчета идеального веса. 
-    if sex == "male":  #  все расчеты в медецине ведутся на идеальный вес, в том чисел потребность в жидкости.
-        ideal_weight = height - 100 - ((height - 150) / 4) 
-    elif sex == "female":  
-        ideal_weight = height - 100 - ((height - 150) / 2)
-    return ideal_weight
-ideal_weight = calc_ideal_weight(user_height_sm, user_sex)
+
 WATER_PER_KG_ADULT = 30
-WATER_PER_KG_OLD = 25
-WATER_PER_KG_VERY_OLD = 20
 ML_PER_LITER = 1000
-water_ml = ideal_weight * WATER_PER_KG_ADULT
-if user_age >= 65 and user_age < 75:
-    water_ml = ideal_weight * WATER_PER_KG_OLD
-elif user_age >= 75:
-    water_ml = ideal_weight * WATER_PER_KG_VERY_OLD  #  если говорить о потребности жидкости, с медицинской точки зрения эти расчеты более точные.
+
+water_ml = user_weight * WATER_PER_KG_ADULT
+
 water_l = round(water_ml / ML_PER_LITER, 2)
+
 print("=" * 62)
 print(f"""Отчет для пользователя: {user_name},{user_age} лет: 
-Ваш индекс массы тела (BMI) составляет: {user_bmi} кг/м². У вас {user_bmi_category}
+Ваш индекс массы тела (BMI) составляет: {user_bmi} кг/м².
 Рекомендуемая норма воды в сутки: {water_l} литра.""")
 print("=" * 62)
 print("Спасибо, что воспользовались FitLife! Берегите себя!")
